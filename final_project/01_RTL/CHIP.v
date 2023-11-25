11528176c3654f171649b531071c9e2a8235b61e
5000user5000 <ck10600760@gmail.com>
2023-11-25 16:52:48 +0800

    bug fixed

--- final_project/01_RTL/CHIP.v
+++ final_project/01_RTL/CHIP.v
@@ -55,7 +55,7 @@ module CHIP #(                                                                  
     parameter ALU_AND = 4'b0000;
     parameter ALU_OR  = 4'b0001;
     parameter ALU_ADD = 4'b0010;
-    parameter ALU_SUB = 3'b0110;
+    parameter ALU_SUB = 4'b0110;
     parameter ALU_SLL = 4'b0111;
     parameter ALU_NOR = 4'b1100;
     parameter ALU_XOR = 4'b1101;
